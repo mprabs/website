5ce3e359c73ea7c56ef94d3654cb6c9c60f6fe38
mprabs <acharyaprabin03@gmail.com>
2020-08-11 11:19:28 +0545

    autoplay

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     // owl-crousel for blog
     $('.owl-carousel').owlCarousel({
         loop: true,
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
